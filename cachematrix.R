9c1053a267ce547594afd75ad968f8f5d5be16b3
thag <prof.raman@live.com>
2015-04-26 13:54:59 -0400

    R Prog Assignment 2 Submission. Ram A Apr 26 2015

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programmer: Ram A, April 26 2015
 
-## Write a short comment describing this function
+## The following two functions allow users to avoid calculating
+## inverse of a matrix by caching the result in a global variable
 
-makeCacheMatrix <- function(x = matrix()) {
+## The following function makeCacheMatrix creates an inverse of a 
+## given matrix as long as the inverse was already not calculated
+## or if the input matrix is empty
+## It assumes that the matrix supplied is always invertible.
 
+makeCacheMatrix <- function(inp = matrix()) {
+    #function to see if the matrices are equal
+    isMatrixEqual <- function(a, b) is.matrix(a) && is.matrix(b) && dim(a) == dim(b) && all(a == b)
+    
+    #if the cache is null or different matrix, calculate inverse and cache
+    if(is.null(matrixOriginal) | (!(isMatrixEqual(matrixOriginal, inp)))){
+        matrixOriginal <<- inp             #store inp as global variable 
+        inverseMatrixCache <<- solve(inp)
+    }
+    else message ("same matrix no need to re-calculate")
+    
 }
 
+## The following function cacheSolve returns inverse of a 
+## given matrix by accessing cache location names inverseMatrixCache
+## as long as the inverse was already calculated.
+## If not it calls the makeCacheMatrix with the supplied matrix.
+## It assumes that the matrix supplied is always invertible.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(inp, ...) {
+    #function to see if the matrices are equal
+    isMatrixEqual <- function(a, b) is.matrix(a) && is.matrix(b) && dim(a) == dim(b) && all(a == b)
+    
+    #if the cache is null or different matrix, calculate inverse by calling makeCacheMatrix 
+    if(is.null(matrixOriginal) | (!(isMatrixEqual(matrixOriginal, inp)))){
+        message ("different matrix, calling makeCacheMatrix()")
+        makeCacheMatrix(inp)
+    }
+    else message ("same matrix retrieve from cache")
+    
+    inverseMatrixCache #return the inverse from the cache
 }
+
+## Test cases
+# m=rbind(c(1, 1), c(-1, 2)) 
+# solve(m)
+# makeCacheMatrix(m)
+# makeCacheMatrix(m)
+# cacheSolve(m)
+# 
+# m=rbind(c(1, -1), c(1, 2)) 
+# solve(m)
+# cacheSolve(m)
+# cacheSolve(m)
+# makeCacheMatrix(m)
+# cacheSolve(m)
+# 
+## http://www.purplemath.com/modules/mtrxinvr.htm
+# m=cbind(c(1,1,1),c(3,4,3),c(3,3,4))
+# cacheSolve(m)
+# cacheSolve(m)
